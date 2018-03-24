045491f40145f0473ef86a25961d077c6351fdae
hK <chaubeyhimanshu241@gmail.com>
2018-03-24 19:48:26 -0400

    spychat third class

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ from spy_details import spy_name, spy_age, spy_raiting
 print 'Hello Buddy'
 print 'Let\'s get started'
 spy_exist = raw_input("Are you a new user? Y/N")
-if spy_exist.upper=="n":
+if spy_exist.upper()=="N":      #use nested if & else.
     print "Welcome Back"
 
-elif spy_exist.upper=="y":
+elif spy_exist.upper()=="Y":     #use .upper for change alphabet foramt in upper case.
     spy_name=raw_input('what is your spy name..!!')
     if len(spy_name)>2:
         print  'welcome '  + spy_name +  '. Glad to have you back with us.'
@@ -14,7 +14,7 @@ elif spy_exist.upper=="y":
             spy_name = spy_salution + "" + spy_name
             print "alright " + spy_name + ". I would like to know a little bit about you"
             spy_age = 0
-            spy_raiting = 0.0 #use for known about spy_qualification
+            spy_raiting = 0.0      #use for known about spy_qualification.
             spy_is_online = True
             spy_age = input('what is your age?')
             if 50>spy_age>12:
