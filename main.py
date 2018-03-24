bda6c6ec2aecd669e48d5beb2448950fd2782c5f
hK <chaubeyhimanshu241@gmail.com>
2018-03-24 17:24:02 -0400

    spy_chat third class

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 print"hello"
-print 'Let\a get started'
+print 'Let\'s get started'
 spy_name=raw_input('what is your spy name..!!')
-
+#use control flow & use nested if else
 if len(spy_name)>2:
     print  'welcome '  + spy_name +  '. Glad to have you back with us.'
     spy_salution = raw_input("what should we call you(Mr. or Ms.)?")
@@ -10,13 +10,13 @@ if len(spy_name)>2:
         spy_name = spy_salution + "" + spy_name
         print "alright " + spy_name + ". I would like to know a little bit about you"
         spy_age = 0
-        spy_raiting = 0.0
+        spy_raiting = 0.0 #use for known about spy_qualification
         spy_is_online = True
         spy_age = input('what is your age?')
         if 50>spy_age>12:
             print "your age is correct"
             spy_raiting = input('what is your raiting?')
-            if spy_raiting>5.0:
+            if spy_raiting>5.0: #nested if 
                 print "great spy"
             elif 3.5<spy_raiting<=5.0:
                 print "avg. spy"
---
+++ spy_details.py
@@ -0,0 +1,2 @@
+spy_name = "guru"
+
